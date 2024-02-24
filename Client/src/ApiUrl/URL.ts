c24f844e1e5292fab750cb684d2ee6917d5c4b32
Oriharel68 <oriharel68@gmail.com>
2024-02-24 17:29:15 +0200

    fixing server url

--- Client/src/ApiUrl/URL.ts
+++ Client/src/ApiUrl/URL.ts
@@ -1,6 +1,6 @@
 
 
 const ApiURL =  "https://clalit-clinics-finder.vercel.app";
-const local = "http://localhost:4000"
+// const local = "http://localhost:4000"
 
-export default local;
+export default ApiURL;
